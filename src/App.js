acab789461567f73e25632074bdf59e8b25d8b2b
SerhiiFront-end <aorl9048@gmail.com>
2023-02-03 11:13:23 +0000

    create header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage/HomePage";
+import ContactPage from "./components/ContactPage/ContactPage";
+import Header from "./UI/Header/Header";
 
+import "./index.css";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/contact" element={<ContactPage />} />
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/" element={<HomePage />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/UI/Header/Header.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom'
+
+import styles from "./Header.module.css"
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <nav className={styles.navigation}>
+                <ul className={styles.listMenu}>
+                   <Link className={styles.LinkItem} to={"/"}><li className={styles.ListItem}>Home</li></Link>
+                   <Link className={styles.LinkItem} to={"/about"}><li className={styles.ListItem}>About Us</li></Link>
+                   <Link className={styles.LinkItem} to={"/aboutProject"}><li className={styles.ListItem}>Our Project</li></Link>
+                   <Link className={styles.LinkItem} to={"/shop"}><li className={styles.ListItem}>Our Shop</li></Link>
+                   <Link className={styles.LinkItem} to={"/help"}><li className={styles.ListItem}>Events</li></Link>
+                   <Link className={styles.LinkItem} to={"/donate"}><li className={styles.ListItem}>Donate</li></Link>
+                   <Link className={styles.LinkItem} to={"/involved"}><li className={styles.ListItem}>Get Involved</li></Link>
+                   <Link className={styles.LinkItem} to={"/contact"}><li className={styles.ListItem}>Contact Us</li></Link>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
